77f14bd779028782777263824a479644f673bb0d
Julian LaNeve <lanevejulian@gmail.com>
2016-03-29 11:54:22 -0500

    Updated for new aries

--- test/test.js
+++ test/test.js
@@ -1,46 +1,30 @@
 import test from 'blue-tape';
 import fs from 'fs';
 import nock from 'nock';
-import salesforce from '..';
-import omit from 'lodash.omit';
+import SalesforceSource from '..';
 import moment from 'moment';
 
 function getTestConfig() {
-    return {
-        user: 'email',
-        pass: 'password' + 'key'
-    }
+	return {
+		user: 'julianlaneve@icloud.com',
+		pass: 'Astronomer2016!' + 'wwQYTUAvCMdly88twvfMR4HF'
+	}
 }
 
 
 test('proper configuration', t => {
-    const activity = salesforce();
-    t.equal(activity.config.name, require('../package.json').name);
-    t.equal(activity.config.version, require('../package.json').version);
-    t.end();
+	const activity = new SalesforceSource();
+	t.equal(SalesforceSource.props.name, require('../package.json').name);
+	t.equal(SalesforceSource.props.version, require('../package.json').version);
+	t.end();
 });
 
 
-test('api request', t => async function () {
-    const activity = salesforce();
-    const config = getTestConfig();
-
-    const result = await activity.request(config.user, config.pass, 'SELECT Id, Name FROM Account');
-    fs.writeFile('log/log.txt', "Result: " + JSON.stringify(result) + '\n');
-    t.ok(result);
-}());
-
-test('s3 upload', t => async function () {
-    const activity = salesforce();
-
-    const key = '12345';
-    const body = '{ "salesforce": "data" }';
-
-    // Intercept request.
-    const scope = nock('https://astronomer-workflows.s3.amazonaws.com')
-        .put(`/${key}`)
-        .reply(200);
-
-    const response = await activity.upload(key, body);
-    t.equal(response.key, key);
+test('api request', t => async function()  {
+	const activity = new SalesforceSource();
+	const config = getTestConfig();
+	const pass = 'Astronomer2016!' + 'wwQYTUAvCMdly88twvfMR4HF';
+	const result = await activity.request("julianlaneve@icloud.com", pass, 'SELECT Id, Name FROM Account');
+	fs.writeFile('log/log.txt', "Result: " + JSON.stringify(result) + '\n');
+	t.ok(result);
 }());
