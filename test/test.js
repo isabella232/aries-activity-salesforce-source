41bcd90e15562a13ca336deb6a5b01b837db0041
Julian LaNeve <lanevejulian@gmail.com>
2016-06-09 10:52:19 -0500

    add select_all operator

--- test/test.js
+++ test/test.js
@@ -5,25 +5,25 @@ import SalesforceSource from '..';
 import moment from 'moment';
 
 function getTestConfig() {
-	return {
-		user: "email_here",
-		pass: 'password' + 'security_token'
-	}
+    return {
+        user: "user/email",
+        pass: 'password' + 'security_token'
+    }
 }
 
 
 test('proper configuration', t => {
-	const activity = new SalesforceSource();
-	t.equal(SalesforceSource.props.name, require('../package.json').name);
-	t.equal(SalesforceSource.props.version, require('../package.json').version);
-	t.end();
+    const activity = new SalesforceSource();
+    t.equal(SalesforceSource.props.name, require('../package.json').name);
+    t.equal(SalesforceSource.props.version, require('../package.json').version);
+    t.end();
 });
 
 
-test('api request', t => async function()  {
-	const activity = new SalesforceSource();
-	const config = getTestConfig();
-	const result = await activity.request(config.user, config.pass, 'SELECT Id, Name FROM Account');
-	fs.writeFile('log/log.txt', "Result: " + JSON.stringify(result) + '\n');
-	t.ok(result);
+test('api request', t => async function() {
+    const activity = new SalesforceSource();
+    const config = getTestConfig();
+    const result = await activity.request(config.user, config.pass, 'SELECT_ALL FROM Account');
+    fs.writeFile('log/log.txt', "Result: " + JSON.stringify(result) + '\n');
+    t.ok(result);
 }());
