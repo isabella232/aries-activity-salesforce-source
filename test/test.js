1f753d7383e5907c824dc066b9562484477da25f
Mike Perry <mike@astronomer.io>
2016-08-01 16:20:18 -0400

    fixed syntax issues in tests

--- test/test.js
+++ test/test.js
@@ -1,33 +1,32 @@
 import test from 'blue-tape';
-import nock from 'nock';
 import SalesforceSource from '..';
 import { ActivityTester } from 'aries-data';
 
 function getTestConfig() {
-    return {
-        username: "username/email",
-        password: 'password' + 'security token',
-        query: 'SELECT_ALL FROM Account'
-    }
+  return {
+    username: 'username/email',
+    password: 'password security token',
+    resourceName: 'Campaign',
+    inclusionFields: [],
+    exclusionFields: [],
+  };
 }
 
-
 test('proper configuration', t => {
-    const activity = new SalesforceSource();
-    t.equal(SalesforceSource.props.name, require('../package.json').name);
-    t.equal(SalesforceSource.props.version, require('../package.json').version);
-    t.end();
+  t.equal(SalesforceSource.props.name, require('../package.json').name);
+  t.equal(SalesforceSource.props.version, require('../package.json').version);
+  t.end();
 });
 
 
-test('api request', t => async function() {
-    const tester = new ActivityTester(SalesforceSource);
+test('api request', async (t) => {
+  const tester = new ActivityTester(SalesforceSource);
 
-    const task = {};
-    const config = getTestConfig();
+  const task = {};
+  const config = getTestConfig();
 
-    tester.testS3StreamOutput('test/output', t.equals);
-    const result = await tester.onTask(task, config);
+  tester.testS3StreamOutput('test/output', t.equals);
+  const result = await tester.onTask(task, config);
 
-    t.ok(result);
-}());
+  t.ok(result);
+});
