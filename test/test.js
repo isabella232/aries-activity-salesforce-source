e8dba6566dd32885633f32e5db1989f791fe0c53
Mike Perry <mike@astronomer.io>
2016-08-02 19:01:49 -0400

    back to wrapping jsforce query in SalesForceStream

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 import test from 'blue-tape';
-import SalesforceSource from '..';
-import { ActivityTester } from 'aries-data';
+import SalesforceSource from '../lib';
+import pkgJson from '../package.json';
+import nock from 'nock';
+
+const loginNock = nock('https://login.salesforce.com').post('/').reply(200, '<serverUrl>https://na1.salesforce.com/services/Soap/u/37.0</serverUrl>');
 
 function getTestConfig() {
   return {
@@ -13,20 +16,20 @@ function getTestConfig() {
 }
 
 test('proper configuration', t => {
-  t.equal(SalesforceSource.props.name, require('../package.json').name);
-  t.equal(SalesforceSource.props.version, require('../package.json').version);
+  t.equal(SalesforceSource.props.name, pkgJson.name);
+  t.equal(SalesforceSource.props.version, pkgJson.version);
   t.end();
 });
 
 
 test('api request', async (t) => {
-  const tester = new ActivityTester(SalesforceSource);
-
-  const task = {};
+  nock.emitter.on('no match', (req) => {
+      t.comment(JSON.stringify(req));
+  });
+  const source = new SalesforceSource();
   const config = getTestConfig();
 
-  tester.testS3StreamOutput('test/output', t.equals);
-  const result = await tester.onTask(task, config);
+  const result = await source.request(config);
 
-  t.ok(result);
+  t.ok();
 });
