4b9aff36f3c4e19c371fba3e0120c0e83f60d187
Dan Sweet <sweetdl@mail.uc.edu>
2017-03-29 12:26:18 -0400

    Added config.query and began rewriting test

--- test/test.js
+++ test/test.js
@@ -1,21 +1,27 @@
-import test from 'blue-tape';
+//import test from 'blue-tape';
+import { assert } from 'chai';
 import SalesforceSource from '../lib';
 import pkgJson from '../package.json';
 import nock from 'nock';
 
+describe('SalesforceSource', () => {
+    describe('#getBlah', () => {
 
-function getTestConfig() {
-    return {
-        username: 'username/email',
-        password: 'password security token',
-        table: 'Campaign',
-        inclusionFields: [],
-        exclusionFields: [],
-    };
-}
-
-test('proper configuration', t => {
-    t.equal(SalesforceSource.props.name, pkgJson.name);
-    t.equal(SalesforceSource.props.version, pkgJson.version);
-    t.end();
+    });
 });
+
+// function getTestConfig() {
+//     return {
+//         username: 'username/email',
+//         password: 'password security token',
+//         table: 'Campaign',
+//         inclusionFields: [],
+//         exclusionFields: [],
+//     };
+// }
+//
+// test('proper configuration', t => {
+//     t.equal(SalesforceSource.props.name, pkgJson.name);
+//     t.equal(SalesforceSource.props.version, pkgJson.version);
+//     t.end();
+// });
