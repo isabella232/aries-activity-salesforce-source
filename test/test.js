28d567f2ef87d0bc7ec87daf75191031335c37b2
Dan Sweet <sweetdl@mail.uc.edu>
2017-04-10 12:52:29 -0400

    Mike did all this

--- test/test.js
+++ test/test.js
@@ -1,14 +1,6 @@
 import { assert } from 'chai';
 import sinon from 'sinon';
 import SalesforceSource from '../lib';
-import pkgJson from '../package.json';
-import jsforce from 'jsforce';
-import nock from 'nock';
-import * as fixtures from './fixtures';
-import config from './test-config.json';
-
-
-const URL = `https://astronomer.lightning.force.com/one/one.app`
 
 describe('SalesforceSource', () => {
     describe('#isBulkQuery', function () {
@@ -20,41 +12,37 @@ describe('SalesforceSource', () => {
     });
 
     describe('#getFields', function () {
-        it('requests all available fields if inclusionFields is empty', function () {
-
+        it('requests all available fields if inclusionFields is empty', async function () {
+            //arrange
+            const source = new SalesforceSource();
+            const mockFields = [{name: 'users'}, {name: 'accounts'}];
+            const description = { fields: mockFields };
+            const describePromise = new Promise(resolve => resolve(description));
+            const describeFn = sinon.stub().returns(describePromise);
+            const sobject = {
+                describe: describeFn
+            };
+            const sobjectFn = sinon.stub().returns(sobject);
+            const conn = {
+                sobject: sobjectFn
+            };
+            const table = 'users';
+            //act
+            const fields = await source.getFields(conn, table);
+            //assert
+            assert.deepEqual(fields, ['users', 'accounts']);
         });
 
-        it('takes the difference of inclusionFields and exclusionFields', function () {
+        it('takes the difference of inclusionFields and exclusionFields', async function () {
+            //arrange
             const inclusionFields = ['field1', 'field2'];
             const exclusionFields = ['field2'];
-            const expect = ['field1'];
-            const actual = //CALL METHOD
-        });
-    });
-
-
-
-
-    describe('#getQuery', () => {
-        it('tests config exists in query', () => {
-            const source = new SalesforceSource;
-            if(config.hasOwnProperty("query")){
-                console.log(config);
-                console.log(config.query);
-            }
-        });
-    });
-
-    describe('#getResponse', () => {
-        before(function(){
-            nock(URL)
-            .get(URL)
-            .reply('200', fixtures.response);
-        });
-        it('tests response', () => {
-            const source = new SalesforceSource;
-            // const data = await index.Query.fields;
-            // assert.ok(data);
+            const table = 'users';
+            const source = new SalesforceSource();
+            //act
+            const actual = await source.getFields(null, table, inclusionFields, exclusionFields);
+            const expected = ['field1'];
+            assert.deepEqual(actual, expected);
         });
     });
 });
